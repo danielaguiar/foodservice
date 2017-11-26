3322f71eb74ed1c59c4147256dad5a966e944aee
daniel aguiar <bsb.aguiar@gmail.com>
2017-11-25 22:06:29 -0200

    adicionando segurança

--- src/main/java/com/gestaosimples/servico/resources/ClienteResource.java
+++ src/main/java/com/gestaosimples/servico/resources/ClienteResource.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,14 +30,14 @@ public class ClienteResource extends AbstractResource {
 
     }
 
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<List<?>> findAll() {
         List<ClienteDTO> clientes = service.findAll();
         return this.okResponseList(clientes);
     }
 
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public Page<ClienteDTO> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page, //
         @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, //
@@ -58,7 +59,7 @@ public class ClienteResource extends AbstractResource {
         return this.noContentResponse();
     }
 
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
